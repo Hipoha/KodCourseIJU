697087c71cde92feb0a4536c08ae9059fd5bc320
Hipoha <reda.mateusz@gmail.com>
2017-12-07 22:27:17 +0100

    10.1. Introduction to Spring [theory]

---
+++ src/main/java/com/kodilla/javacourse/JavacourseApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla.javacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacourseApplication.class, args);
+	}
+}
