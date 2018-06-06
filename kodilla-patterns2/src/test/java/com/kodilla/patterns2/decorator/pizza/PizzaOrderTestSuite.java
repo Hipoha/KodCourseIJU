32a840b1e1d87878981ddf742b38e0b5289f7a2d
Meteo-R <reda.mateusz@gmail.com>
2018-06-06 23:29:07 +0200

    AbstractPizzaOrderDecorator and BasicPizzaOrder + test
    20.2. Design patterns: decorator [exercise 2]

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
@@ -1,4 +1,19 @@
 package com.kodilla.patterns2.decorator.pizza;
 
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
 public class PizzaOrderTestSuite {
+    @Test
+    public void testBasicPizzaOrder() {
+        //Given
+        PizzaOrder pizzaOrder = new BasicPizzaOrder();
+        //When
+        //Then
+        assertEquals(new BigDecimal(15), pizzaOrder.getCost());
+        assertEquals("Order: Pizza", pizzaOrder.getDescription());
+    }
 }
