52b4a9e91cade9deff4e91eec3de10f801a7fe31
Meteo-R <reda.mateusz@gmail.com>
2018-06-23 13:08:25 +0200

    Calling SQL procedure from Java [exercise]
    22.3. Databases: supplementary info [exercise]

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -30,4 +30,24 @@ public class StoredProcTestSuite {
         assertEquals(0, howMany);
     }
 
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "update books set bestseller=null";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        //When
+        String sqlProcedureCall = "call UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+        //Then
+        String sqlCheckTable = "select (count(*) - count(bestseller)) as how_many from books;";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()) {
+            howMany = rs.getInt("how_many");
+        }
+        assertEquals(0, howMany);
+    }
+
 }
