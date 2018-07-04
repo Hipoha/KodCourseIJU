d53d66bfb8cbe99a065035b006027a70731e9ffb
Meteo-R <reda.mateusz@gmail.com>
2018-07-05 00:51:21 +0200

    'testing' module imported

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,29 @@
+package com.kodilla.testing;
+
+import com.kodilla.testing.shape.Circle;
+import com.kodilla.testing.shape.ShapeCollector;
+import com.kodilla.testing.shape.Square;
+import com.kodilla.testing.shape.Triangle;
+
+public class TestingMain {
+    public static void main(String[] args){
+
+        Circle circle = new Circle(1.0);
+        Square square = new Square(2.0);
+        Triangle triangle = new Triangle(2.0,2.0);
+
+        ShapeCollector test = new ShapeCollector(
+//                new ArrayList<>(
+//                        Arrays.asList(
+//                        circle,
+//                        square,
+//                        triangle
+//                ))
+        );
+
+
+
+
+
+    }
+}
